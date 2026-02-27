287a108f8983d785ea057f6bf97a54c7dc7c26fd
laptop <penis@gmail.com>
2026-02-27 22:16:40 +0400

    react app 1

--- lerning-react/src/App.js
+++ lerning-react/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import VideoList from './components/VideoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>App compoent</h1>
+      <VideoList />
+    </>
+  )
 }
 
 export default App;
